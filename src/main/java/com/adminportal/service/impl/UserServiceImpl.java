8000008765ac5a508ddd8067f03f8995abebf8b1
tiansss <tianshi.st1020@gmail.com>
2018-12-12 12:49:46 -0500

    log in functionality done

--- src/main/java/com/adminportal/service/impl/UserServiceImpl.java
+++ src/main/java/com/adminportal/service/impl/UserServiceImpl.java
@@ -24,25 +24,7 @@ public class UserServiceImpl implements UserService{
 	
 	@Autowired
 	private RoleRepository roleRepository;
-	
-	@Autowired
-	private PasswordResetTokenRepository passwordResetTokenRepository;
-	
-	@Override
-	public PasswordResetToken getPasswordResetToken(final String token) {
-		return passwordResetTokenRepository.findByToken(token);
-	}
-	
-	@Override
-	public void createPasswordResetTokenForUser(final User user, final String token) {
-		final PasswordResetToken myToken = new PasswordResetToken(token, user);
-		passwordResetTokenRepository.save(myToken);
-	}
-	
-	@Override
-	public User findByUsername(String username) {
-		return userRepository.findByUsername(username);
-	}
+
 	
 	@Override
 	public User createUser(User user, Set<UserRole> userRoles) throws Exception{
